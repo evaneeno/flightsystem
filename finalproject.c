43486171653ee573be51948f223faeaeafbdbbaf
evaneeno <75227655+evaneeno@users.noreply.github.com>
2020-11-29 16:55:23 -0700

    Update finalproject.c

--- finalproject.c
+++ finalproject.c
@@ -1,5 +1,5 @@
 /*
-Title: Flight Management system
+Title: Flight Management system (TEST)
 Desc: This program contains information about flights and lets users book flights
 Name: Addison Wenzel, Zac Chen, Andy Lin, Chris DeMuyt, Evan Michael Harding
 Email: wenzela@purdue.edu, lin1019@purdue.edu, chen2946@purdue.edu, cdemuyt@purdue.edu, hardine@purdue.edu
